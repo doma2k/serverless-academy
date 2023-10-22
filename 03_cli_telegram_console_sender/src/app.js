92474a8fd8a7b27783aee56ed5ee00f841ad9997
doma2k <deep.web2018@yahoo.com>
2023-10-22 19:30:33 +0100

    feat: add messege

--- 03_cli_telegram_console_sender/src/app.js
+++ 03_cli_telegram_console_sender/src/app.js
@@ -1,39 +1,32 @@
-import { Command } from 'commander';
-import TelegramBot from 'node-telegram-bot-api';
+import { Command } from "commander";
+import TelegramBot from "node-telegram-bot-api";
 
-const token = '6286796097:AAGxpV-uS_ly30_I3Q_8m8bRHGkcCcNP1AQ';
-const chatId = 1933874030
+const token = "5869901140:AAEqaOSk5ggcpNdCYHz9AmIKQIgOyXlD-r8";
+const chatId = 1933874030;
 const program = new Command();
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token);
 
-program
-  .name('app.js')
-  .description('Serverless school practice.')
-  .version('0.0.1');
+async function app() {
+  program
+    .name("app.js")
+    .description("Serverless school practice.")
+    .version("0.0.1");
 
-program.command('send-message')
-       .description('Send message in to your Telegram bot from terminal.')
-   .argument('<string>', 'Type your mesage here.')
-  .action((str) => {
-    try{
-      bot.sendMessage(chatId, str);
-    }catch(err){
-      console.log("some error", err)
-    } 
-         console.log(str.split());
-         
-              process.exit(0)
-  });
+  program
+    .command("send-message")
+    .description("Send message in to your Telegram bot from terminal.")
+    .argument("<message>", "Type your message here.")
 
-program.command('send-photo')
-  .description('Send picture in to your Telegram bot from terminal.')
-  .argument('<string>', 'file location,example: "/path/to/the/photo.png" ')
-  .option('--first', 'display just the first substring')
-  .action((str) => {
-    console.log(str.split())
-  });
+  program
+    .command("send-photo")
+    .description("Send picture in to your Telegram bot from terminal.")
+    .argument("<string>", 'file location,example: "/path/to/the/photo.png" ')
 
-program.parse();
+  const appArgs = program.parse();
 
+  if (appArgs.args[0] === "send-message") {
+    bot.sendMessage(chatId, appArgs.args[1])
+  }
+}
 
-
+app();
