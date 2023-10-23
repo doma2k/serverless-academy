704a2535a25b46260cc847d53a81c835d6655049
doma2k <deep.web2018@yahoo.com>
2023-10-23 15:15:20 +0100

    Feat: optMizeeee

--- 04_telegram_bot_weather_forecast/src/app.js
+++ 04_telegram_bot_weather_forecast/src/app.js
@@ -7,89 +7,81 @@ const bot = new TelegramBot(BOT_ID, { polling: true });
 const intervalRefs = {};
 let city = "Cascais";
 
-async function msgSend(chatId, weatherData) {
-  bot.sendMessage(chatId, weatherData);
+async function sendMessage(chatId, text, keyboard) {
+  await bot.sendMessage(chatId, text, keyboard);
 }
 
-async function getData() {
-  const geoGet = await axios({
-    method: "get",
-    url: `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${WEATHER_ID}`,
-    responseType: "json",
-  });
+async function getWeatherData() {
+  const geoResponse = await axios.get(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${WEATHER_ID}`
+  );
+  const location = geoResponse.data[0];
+  const lat = location.lat.toFixed(2);
+  const lon = location.lon.toFixed(2);
+  const country = location.country;
 
-  const cityName = geoGet.data[0].name;
-  const lat = geoGet.data[0].lat.toFixed(2);
-  const lon = geoGet.data[0].lon.toFixed(2);
-  const country = geoGet.data[0].country;
-  const wetherGet = await axios({
-    method: "get",
-    url: `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${WEATHER_ID}`,
-    responseType: "json",
-  });
-  const weatherDAta = wetherGet.data.list[(0, 1)];
-  const temp = weatherDAta.main.temp;
-  const weather = weatherDAta.weather;
-  let weatherMessage;
-  return (weatherMessage = `${cityName} ${country}\nWeather: ${weather[0].main}\nDescription: ${weather[0].description}\nTempreture: ${temp}°C\n`);
+  const weatherResponse = await axios.get(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${WEATHER_ID}`
+  );
+  const weatherData = weatherResponse.data.list[0];
+  const temp = weatherData.main.temp;
+  const weather = weatherData.weather;
+
+  return `${location.name} ${country}\nWeather: ${weather[0].main}\nDescription: ${weather[0].description}\nTemperature: ${temp}°C`;
 }
 
-async function wBot() {
-  bot.onText(/\/start/, (msg) => {
-    const chatId = msg.chat.id;
-    const message = "Welcome to the Weather Forecast bot:";
-    const keyboard = {
-      reply_markup: {
-        keyboard: [["Forecast in Cascais"]],
-        one_time_keyboard: true,
-      },
-    };
-    if (intervalRefs[chatId]) {
-      clearInterval(intervalRefs[chatId]);
-    }
-    bot.sendMessage(chatId, message, keyboard);
-  });
-
-  bot.onText(/Forecast in Cascais/, async (msg) => {
-    const chatId = msg.chat.id;
-    const keyboard = {
-      reply_markup: {
-        keyboard: [["at intervals of 3 hours"], ["at intervals of 6 hours"]],
-        one_time_keyboard: true,
-      },
-    };
-    await bot.sendMessage(
-      chatId,
-      "This is current forecast:",
-    );
-    const weatherData = await getData();
-    await msgSend(chatId, weatherData);
-    await bot.sendMessage(
-      chatId,
-      "Please select the forecast interval:",
-      keyboard
-    );
-  });
-
-  bot.onText(/at intervals of 3 hours|at intervals of 6 hours/, (msg) => {
-    const chatId = msg.chat.id;
-    if (intervalRefs[chatId]) {
-      clearInterval(intervalRefs[chatId]);
-    }
-    if (msg.text === "at intervals of 3 hours") {
-      const interval = 3 * 60 * 60 * 1000;
-      intervalRefs[chatId] = setInterval(async () => {
-        const weatherData = await getData();
-        await msgSend(chatId, weatherData);
-      }, interval);
-    } else if (msg.text === "at intervals of 6 hours") {
-      const interval = 6 * 60 * 60 * 1000;
-      intervalRefs[chatId] = setInterval(async () => {
-        const weatherData = await getData();
-        await msgSend(chatId, weatherData);
-      }, interval);
-    }
-  });
+function startHandler(msg) {
+  const chatId = msg.chat.id;
+  const message = "Welcome to the Weather Forecast bot:";
+  const keyboard = {
+    reply_markup: {
+      keyboard: [["Forecast in Cascais"]],
+      one_time_keyboard: true,
+    },
+  };
+  if (intervalRefs[chatId]) {
+    clearInterval(intervalRefs[chatId]);
+  }
+  sendMessage(chatId, message, keyboard);
 }
 
-wBot();
+async function forecastHandler(msg) {
+  const chatId = msg.chat.id;
+  const keyboard = {
+    reply_markup: {
+      keyboard: [["at intervals of 3 hours", "at intervals of 6 hours"]],
+      one_time_keyboard: true,
+    },
+  };
+  await sendMessage(chatId, "This is the current forecast:");
+  const weatherData = await getWeatherData();
+  await sendMessage(chatId, weatherData);
+  await sendMessage(chatId, "Please select the forecast interval:", keyboard);
+}
+
+function intervalHandler(msg) {
+  const chatId = msg.chat.id;
+  if (intervalRefs[chatId]) {
+    clearInterval(intervalRefs[chatId]);
+  }
+
+  let interval;
+  if (msg.text === "at intervals of 3 hours") {
+    interval = 3 * 60 * 60 * 1000;
+  } else if (msg.text === "at intervals of 6 hours") {
+    interval = 6 * 60 * 60 * 1000;
+  }
+
+  intervalRefs[chatId] = setInterval(async () => {
+    const weatherData = await getWeatherData();
+    await sendMessage(chatId, weatherData);
+  }, interval);
+}
+
+function weatherBot() {
+  bot.onText(/\/start/, startHandler);
+  bot.onText(/Forecast in Cascais/, forecastHandler);
+  bot.onText(/at intervals of 3 hours|at intervals of 6 hours/, intervalHandler);
+}
+
+weatherBot();
