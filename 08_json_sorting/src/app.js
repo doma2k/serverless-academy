f2383d9129dba7a491ab4710af449de2de2bca94
doma2k <deep.web2018@yahoo.com>
2023-10-31 11:53:33 +0000

    feat: done

--- 08_json_sorting/src/app.js
+++ 08_json_sorting/src/app.js
@@ -1,5 +1,8 @@
 const reqtimes = 3;
-const apiList = [
+let countFalse = 0;
+let countTrue = 0;
+let resp;
+const apiListx = [
   "https://jsonbase.com/sls-team/json-793",
   "https://jsonbase.com/sls-team/json-955",
   "https://jsonbase.com/sls-team/json-231",
@@ -22,10 +25,33 @@ const apiList = [
   "https://jsonbase.com/sls-team/json-64",
 ];
 
+const apiList = [
+  "http://localhost:3000/sls-team/json-793",
+  "http://localhost:3000/sls-team/json-955",
+  "http://localhost:3000/sls-team/json-231",
+  "http://localhost:3000/sls-team/json-931",
+  "http://localhost:3000/sls-team/json-93",
+  "http://localhost:3000/sls-team/json-342",
+  "http://localhost:3000/sls-team/json-770",
+  "http://localhost:3000/sls-team/json-491",
+  "http://localhost:3000/sls-team/json-281",
+  "http://localhost:3000/sls-team/json-718",
+  "http://localhost:3000/sls-team/json-310",
+  "http://localhost:3000/sls-team/json-806",
+  "http://localhost:3000/sls-team/json-469",
+  "http://localhost:3000/sls-team/json-258",
+  "http://localhost:3000/sls-team/json-516",
+  "http://localhost:3000/sls-team/json-79",
+  "http://localhost:3000/sls-team/json-706",
+  "http://localhost:3000/sls-team/json-521",
+  "http://localhost:3000/sls-team/json-350",
+  "http://localhost:3000/sls-team/json-64",
+];
+
 async function parceJson(api, requests) {
   let response;
   let data;
-
+  
   for (let i = 0; i < requests; i++) {
     response = await fetch(api);
     if (response && response.ok) {
@@ -35,14 +61,44 @@ async function parceJson(api, requests) {
   }
 
   if (!response || !response.ok) {
-    return `[FAILED]`;
+    return { result: `[FAILED]`, data: null };
   } else {
-    return `[SUCCESS]`;
+    const resp = findIsDone(data);
+    return { result: `[SUCCESS]`, data: resp };
   }
 }
 
-for (let i = 0; i < apiList.length; i++) {
-  const response = await parceJson(apiList[i]);
-  console.log(response + " " + apiList[i]);
+function findIsDone(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] === 'object') {
+      const result = findIsDone(obj[key]);
+      if (result !== null) {
+        return result;
+      }
+    } else if (key === 'isDone' && typeof obj[key] === 'boolean') {
+      return obj;
+    }
+  }
+  return null; 
 }
 
+async function processAPIs() {
+  for (let i = 0; i < apiList.length; i++) {
+    const { result, data } = await parceJson(apiList[i], reqtimes);
+    if (data) {
+      console.log(`${result} ${apiList[i]}: isDone - ${data.isDone}`);
+      if (data.isDone === true) {
+        countTrue++;
+      } else {
+        countFalse++;
+      }
+    } else {
+      console.log(`${result} ${apiList[i]}: isDone not found.`);
+    }
+  }
+  
+  console.log(`Count of 'isDone' true: ${countTrue}`);
+  console.log(`Count of 'isDone' false: ${countFalse}`);
+}
+
+processAPIs();
